51fbc77a67b74c8d3d8378426d84941b56807404
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-10-19 22:58:14 +0200

    ;ug

--- train_squad.py
+++ train_squad.py
@@ -3,10 +3,16 @@ import json
 from typing import List, Tuple
 
 from torch.utils import data
-from utils import tokenize, stemming, bag_of_words, get_sorted_unique_string_list, get_training_device
+from utils import (
+    tokenize,
+    stemming,
+    bag_of_words,
+    get_sorted_unique_string_list,
+    get_training_device,
+)
 from copy import deepcopy
 
-import torch 
+import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, dataset
 
@@ -17,26 +23,27 @@ import numpy as np
 from squad import *
 from constants import *
 
-SQUAD_FILE_PATH: str = 'squad_dataset.json'
+SQUAD_FILE_PATH: str = "squad_dataset.json"
 
-class TrainData():
-    
+
+class TrainData:
     def __init__(self) -> None:
 
-        self.ignore_words = ['?', '!', '.', ',', ';']
+        self.ignore_words = ["?", "!", ".", ",", ";"]
         self.all_words = []
         self.X_y = []
         self.tags = []
-        
 
-        with open(SQUAD_FILE_PATH, 'r') as file:
+        with open(SQUAD_FILE_PATH, "r") as file:
             squad_json = json.load(file)
 
             squad = Squad(squad_json)
 
             squad_data_list = squad.data_list
 
+            print("get tags, all words and X_y set")
             for squad_data in squad_data_list:
+
                 tag = squad_data.title
                 self.tags.append(tag)
                 for paragraph in squad_data.paragraphs:
@@ -48,15 +55,25 @@ class TrainData():
                         self.all_words.extend(question)
                         self.X_y.append((question, tag))
 
-            self.all_words = [stemming(word) for word in self.all_words if word not in self.ignore_words]
+            print("stem all words")
+            self.all_words = [
+                stemming(word)
+                for word in self.all_words
+                if word not in self.ignore_words
+            ]
             self.all_words = get_sorted_unique_string_list(self.all_words)
             self.tags = get_sorted_unique_string_list(self.tags)
 
             self.X_train = []
             self.y_train = []
 
+            print("create bag of words")
+
+            all_words_dict = self.get_all_words_dict()
+            # del self.all_words
+
             for (pattern_sentence, tag) in self.X_y:
-                bag = bag_of_words(pattern_sentence, self.all_words)
+                bag = bag_of_words(pattern_sentence, all_words_dict)
                 self.X_train.append(bag)
 
                 label = self.tags.index(tag)
@@ -68,9 +85,16 @@ class TrainData():
     def get_X_y_train(self):
         return self.X_train, self.y_train
 
+    def get_all_words_dict(self) -> Dict[str, int]:
+        all_words_dict = {}
+
+        for idx, word in enumerate(self.all_words):
+            all_words_dict[word] = idx
+
+        return all_words_dict
+
 
 class ChatDataSet(Dataset):
-
     def __init__(self, X_train, y_train) -> None:
         super().__init__()
         self.n_samples = len(X_train)
@@ -81,32 +105,28 @@ class ChatDataSet(Dataset):
     def __getitem__(self, index) -> Tuple:
         return self.X_data_[index], self.y_data_[index]
 
-
     def __len__(self) -> int:
         return self.n_samples
-
-            
 
 
 def main():
     train_data = TrainData()
 
     # Hyperparameters
-    batch_size: int = 16
+    batch_size: int = 32
     input_size = len(train_data.all_words)
-    hidden_size = 16
+    hidden_size = 128
     output_size = len(train_data.tags)
     learning_rate = 0.001
     num_epoch = 1000
 
-    X_train, y_train= train_data.get_X_y_train()
+    X_train, y_train = train_data.get_X_y_train()
 
     dataset = ChatDataSet(X_train, y_train)
 
-    train_loader = DataLoader(dataset=dataset, 
-                              batch_size=batch_size, 
-                              shuffle=True, 
-                              num_workers=0)
+    train_loader = DataLoader(
+        dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=5
+    )
 
     device = get_training_device()
     model = NeuralNet(input_size, hidden_size, output_size).to(device)
@@ -115,15 +135,15 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    print('Start training')
-    
+    print("Start training")
+
     # training loop
     for epoch in range(num_epoch):
         for (words, labels) in train_loader:
             words = words.to(device)
             labels = labels.to(dtype=torch.long).to(device)
 
-            # forward 
+            # forward
             outputs = model(words)
             # if y would be one-hot, we must apply
             # labels = torch.max(labels, 1)[1]
@@ -135,9 +155,9 @@ def main():
             optimizer.step()
 
         if (epoch + 1) % 100 == 0:
-            print(f'epoch {epoch + 1}/{num_epoch}, loss={loss.item():4f}')
+            print(f"epoch {epoch + 1}/{num_epoch}, loss={loss.item():4f}")
 
-    print(f'final loss={loss.item():4f}')
+    print(f"final loss={loss.item():4f}")
 
     data = {
         MODEL_STATE: model.state_dict(),
@@ -145,13 +165,14 @@ def main():
         OUTPUT_SIZE: output_size,
         HIDDEN_SIZE: hidden_size,
         ALL_WORDS: train_data.all_words,
-        TAGS: train_data.tags
+        TAGS: train_data.tags,
     }
 
-    file_name = 'data.pth'
+    file_name = "data.pth"
     torch.save(data, file_name)
 
-    print(f'training complete. file saved to {file_name}')
+    print(f"training complete. file saved to {file_name}")
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -3,33 +3,44 @@ from typing import List
 import nltk
 import numpy as np
 import torch
+
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
 stemmer = PorterStemmer()
 
+
 def tokenize(sentence: str) -> List[str]:
     return nltk.word_tokenize(sentence)
+
 
 def stemming(word: str) -> str:
     return stemmer.stem(word.lower())
 
+
 def get_sorted_unique_string_list(word_list: List[str]) -> List[str]:
     return sorted(set(word_list))
 
-def bag_of_words(tokenized_sentence, all_words) -> List[str]:
+
+def bag_of_words(tokenized_sentence, all_words_dict: dict) -> List[str]:
     tokenized_sentence = [stemming(word) for word in tokenized_sentence]
 
-    bag = np.zeros(len(all_words), dtype=np.float32)
+    bag = np.zeros(len(all_words_dict.keys()), dtype=np.float32)
 
-    for idx, word in enumerate(all_words):
-        if word in tokenized_sentence:
+    for word in tokenized_sentence:
+        if word in all_words_dict:
+            idx = all_words_dict[word]
             bag[idx] = 1
+
+    # for idx, word in enumerate(all_words):
+    #     if word in tokenized_sentence:
+    #         bag[idx] = 1
 
     return bag
 
+
 def get_training_device():
-    return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 # words = ['Organize', 'organizes', 'organization', 'organizing']
