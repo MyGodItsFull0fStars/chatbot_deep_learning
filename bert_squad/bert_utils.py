b3fd75bcbcdf260bc808cb77a3d587b353f23e8d
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-02-12 21:26:32 +0100

    Add bert to repository

---
+++ bert_squad/bert_utils.py
@@ -0,0 +1,26 @@
+from transformers import BertForQuestionAnswering, BertTokenizer
+import torch
+
+
+class BertUtils():
+
+    def __init__(self, model: BertForQuestionAnswering = None, tokenizer: BertTokenizer = None):
+        """Initializes the Utility Class for BERT
+
+        Args:
+            model (BertForQuestionAnswering, optional): model used for BERT classification. Defaults to None.
+            tokenizer (BertTokenizer, optional): tokenizer used for BERT classification. Defaults to None.
+        """
+
+        self.pretrained_model_name: str = 'bert-large-uncased-whole-word-masking-finetuned-squad'
+        if model is None:
+            model = BertForQuestionAnswering.from_pretrained(
+                self.pretrained_model_name)
+
+        if tokenizer is None:
+            tokenizer = BertTokenizer.from_pretrained(
+                self.pretrained_model_name)
+
+
+def get_answer_to_question(tokenizer, question: str, answer_text: str, print_output: bool = False) -> str:
+    input_ids = tokenizer.encode(question, answer_text)
